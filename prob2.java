d44515daeca29997aa7aed5ca41cad7e2420262c
gunjalharsh <41323871+gunjalharsh@users.noreply.github.com>
2020-01-23 19:09:40 +0530

    Add files via upload

--- prob2.java
+++ prob2.java
@@ -1,4 +1,4 @@
-import java.util.Scanner*;
+import java.util.*;
  class prob2
 {
 
@@ -7,19 +7,21 @@ public static void main(String args[])
 
 Scanner choco=new Scanner(System.in);
 System.out.println("Enter the choco = ");
-int chocono=choco.nextINT();
+int chocono=choco.nextInt();
 
  
 Scanner children=new Scanner(System.in);
 System.out.println("Enter the children = ");
-int childno=children.nextINT();
-
-for(i=1;i<=childno;i++)
+int childno=children.nextInt();
+int i,j=0;
+for(i=1;i <=childno;i++)
 {
-for(j= )
 
-
+for( j=chocono;j>1;j=j-1)
+{
+chocono=chocono-1;
 }
 
 }
-System.out.println("Chocolate left = ");
+
+System.out.println("Chocolate left  " +j);}}
